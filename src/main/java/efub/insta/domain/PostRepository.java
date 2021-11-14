315a8a06efb872ee9c3d79c37f6d2a3de1d4d202
MINJU-KIMmm <0112km@ewhain.net>
2021-11-14 21:10:24 +0900

    Feat : "유저도메인, 포스트도메인 생성"

---
+++ src/main/java/efub/insta/domain/PostRepository.java
@@ -0,0 +1,6 @@
+package efub.insta.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository {
+}
---
+++ src/main/java/efub/insta/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package efub.insta.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+}
