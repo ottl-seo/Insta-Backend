69de22017f25ebe09c62d8053a9a882be4fdc2d6
ottl-seo <dotsi@naver.com>
2021-12-01 16:48:48 +0900

    Feat: 채팅 pub/sub 기능 추가

--- src/main/java/efub/insta/controller/WebSecurityConfig.java
+++ src/main/java/efub/insta/controller/WebSecurityConfig.java
@@ -1,15 +1,19 @@
-package efub.insta.config;
+package efub.insta.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() { //cors 정책 설정
 
@@ -24,5 +28,11 @@ public class WebSecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/ws-stomp/info", "/ws-stomp/**")
+                .permitAll();
     }
 }
