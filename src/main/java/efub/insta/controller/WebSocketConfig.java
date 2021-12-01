7c658aa9c0503700beadef0f66ac0e73106e1133
ottl-seo <dotsi@naver.com>
2021-12-01 19:00:24 +0900

    #13 Fix: websocket CORS 오류 해결

--- src/main/java/efub/insta/controller/WebSocketConfig.java
+++ src/main/java/efub/insta/controller/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("http://ec2-3-36-132-41.ap-northeast-2.compute.amazonaws.com", "http://localhost:3000", "http://localhost:8080")
                 .withSockJS();
-    }  // 이제 websocket 연결 엔드포인트가 ws://localhost:8080/ws-stomp 가 됨
+    }  // 이제 websocket 연결 엔드포인트가 ws://localhost:8080/ws-stomp 가 됨 //allowedOriginPatterns
 }
